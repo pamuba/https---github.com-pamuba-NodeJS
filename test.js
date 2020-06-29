b1081e69a19c104e891aa3cfa581fb6002ff0667
pamuba <pamuba@users.noreply.github.com>
2020-06-30 00:32:30 +0530

    Co-authored-by: Xiaowen Tan <xiaowent@usc.edu> Co-authored-by: Vodapally, Ravitheja <vodapallyr7654@uhcl.edu> Co-authored-by: bl2684@nyu.edu <bl2684@nyu.edu> Co-authored-by: sarahlighter <sgjybm@outlook.com> Co-authored-by: Changsheng Liu <liucs90@hotmail.com> Co-authored-by: Chi Luo <luochi94@gmail.com> Co-authored-by: jiangzhi1995@gmail.com <jiangzhi1995@gmail.com> Co-authored-by: 415365348@qq.com <415365348@qq.com> Co-authored-by: Yangming Ke <yangmingk@gmail.com>

--- test.js
+++ test.js
@@ -8,14 +8,56 @@
 
 // myLog.log("Prince")
 
-var fs = require('fs')
+// var fs = require('fs')
 //Async
-fs.readFile('data.txt', function(err, data){
-    if(err){
-        return console.log(err)
-    }
-    else
-    {
-        console.log(data.toString())
-    }
+// fs.readFile('data.txt', function(err, data){
+//     if(err){
+//         return console.log(err)
+//     }
+//     else
+//     {
+//         console.log(data.toString())
+//     }
+// })
+
+// //sync 
+// var data = fs.readFileSync('data.txt');
+// console.log(data.toString())
+
+
+//Admin
+
+//Normal
+
+// var fs = require('fs')
+// fs.open('data.txt','r', function(err, fd){
+//     if(err){console.log(err)}
+//     else{
+//         var buffer = new Buffer.alloc(1024);
+//         fs.read(fd, buffer, 2, 1022, 5, function(err, bytes){
+//             //Print
+//             // console.log(buffer.slice(0, bytes).toString())
+//             console.log(buffer)
+//         })
+//         fs.close(fd, function(err){
+//             console.log(err)
+//         })
+
+//         ///fs.write
+//         // fs.wr
+//     }
+// })
+
+
+ var fs = require('fs')
+// fs.writeFile('text.txt', "Hello World", function(err){
+//     if(err){console.log(err)}
+//     else{console.log("Data Written successfully")}
+// })
+
+
+fs.readFile('data.txt', 'utf-8', function(err, data){
+
 })
+
+
